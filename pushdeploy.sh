48e2e2f100c8afc38f49900b194937875d2eac45
Andreas Grabner <grabnerandi@gmx.at>
2018-08-02 11:06:05 +0200

    fixing script

--- pushdeploy.sh
+++ pushdeploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Will call the Dynatrace CLI in Docker and push deployment information for the host that is specified in monspec/monspec.json
 # we assume that Dynatrace CLI is installed in ../dynatrace_cli
+# Usage: ./pushdeploy.sh EntityName/Environment [DeploymentName] [Version]
+# Example /pushdeploy.sh MaaSHost/Lab2 DeployMachine InitialVersion  -> will push a deployment on the host as defined in MaaShost/Lab2
+# Example /pushdeploy.sh FrontendApp/Dev DeployService Version1  -> will push a deployment on the service as defined in FrontendApp/Dev
 
 if [ -z "$1" ]; then
   echo "Usage: Arg 1 needs to be an EntityName/Environment combination, e.g: MaaSHost/Lab2 or FrontendApp/Dev"
